4b3c5a9e8f7160228581e05fb5041e80a39ff310
Rangga mahardika putra santoso <46999602+Ranggamps@users.noreply.github.com>
2019-01-24 22:43:31 +0700

    tambah methid cari luas

--- hitungluaspersegi.java
+++ hitungluaspersegi.java
@@ -1,6 +1,9 @@
 // Menghitung luas persegipanjang
 
 public class tugas1{
+static int cariLuasPersegiPanjang(int panjang, int lebar){
+return panjang*lebar;
+}
 	public static void main(String[]args){
 	
 	}
