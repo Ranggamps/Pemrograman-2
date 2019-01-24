2ae019a93b64df47212d46bc66f6c1d1ba80e81d
Rangga mahardika putra santoso <46999602+Ranggamps@users.noreply.github.com>
2019-01-24 22:54:09 +0700

    cariluaslingkaran

--- hitungluaspersegi.java
+++ hitungluaspersegi.java
@@ -4,6 +4,10 @@ public class tugas1{
 static int cariLuasPersegiPanjang(int panjang, int lebar){
 return panjang*lebar;
 }
+static int cariLuasLingkaran(int jarijari){
+return panjang*lebar;
+}
+
 	public static void main(String[]args){
 	
 	}
